ce12a23b1765c86289f2f353e192dd172ead1201
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-11-19 22:20:32 +0000

    new question added

--- models/quiz_model2.js
+++ models/quiz_model2.js
@@ -7,10 +7,14 @@ var Respuesta = require('../models/respuesta.js');
 function Quiz() {
   AbstractQuiz.call(this);
   this.q.push(
+
     { pregunta: new PCorta('¿Capital de Italia?'),
       respuesta: new Respuesta(/^\s*roma\s*$/i)
+    },
+
+    { pregunta: new PLarga('¿Quien reinaba en España cuando se descubrió América?'),
+      respuesta: new Respuesta(/\b(Isabel\s+y?\s*Fernando)|(Fernando\s+[ey]?\s*Isabel)|(reyes\s+cat[oó]licos)\b/i)
     }
-  // debug(this.q);
   );
 }
 
