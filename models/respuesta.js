b333c15b7060842b4b2dd5604891878156dee749
José Saúl Giffuni <giffunis@gmail.com>
2015-11-20 09:54:23 +0000

    solved the respuesta class

--- models/respuesta.js
+++ models/respuesta.js
@@ -1,18 +1,20 @@
 "use stric";
 
 function Respuesta(respuesta){
-  this.respuesta = respuesta;
 
-
-  if(typeof(this.respuesta) === 'string' || typeof(this.respuesta) === 'number') {
-    return function(x){return x === respuesta;};
-  }else if (this.respuesta.__proto__.toString() === '/(?:)/') {
+ var f;
+  if(typeof(respuesta) === 'string' || typeof(respuesta) === 'number') {
+    f =  function(x){return x === respuesta;};
+  }else if (respuesta.__proto__.toString() === '/(?:)/') {
     // var aux = this.respuesta;
-    return function(x){return x.match(respuesta);};
+    f =  function(x){return x.match(respuesta);};
   }
-  else if (this.respuesta.__proto__.toString() === "function () {}") {
-    return respuesta;
+  else  {
+    f =  respuesta;
   }
+
+  f.__proto__ = this.__proto__;
+  return f;
 }
 
 
